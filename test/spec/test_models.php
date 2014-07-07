5ed115d4bfeb95b47fdaaed76b55d3ef8e49a1a5
Sérgio Vilar <sergiovilar.r@gmail.com>
2014-07-06 22:30:09 -0300

    Added some methods to test models

--- test/spec/test_models.php
+++ test/spec/test_models.php
@@ -8,6 +8,9 @@
 
 class WP_Test_Alter_Models extends Alter_UnitTestCase {
 
+    /**
+     * This method tests if the model has been loaded
+     */
     function test_models_loaded(){
 
         $this->assertTrue(class_exists('BookModel'), 'Verify if the class BookModel has been loaded');
@@ -15,6 +18,9 @@ class WP_Test_Alter_Models extends Alter_UnitTestCase {
 
     }
 
+    /**
+     * This method tests if the post type has been registered
+     */
     function test_post_type_exists(){
 
         do_action('init');
@@ -23,9 +29,42 @@ class WP_Test_Alter_Models extends Alter_UnitTestCase {
 
     }
 
-    function test_models_return(){
+    /**
+     * This method tests if in a empty database, the return of find() will be false
+     */
+    function test_models_empty_return(){
 
         $this->assertFalse(is_array($this->app->book->find()), 'Check if the model->find() method returns false (empty)');
+
+    }
+
+    /**
+     * This method test the attributes of the PostObject
+     */
+    function test_models_return(){
+
+        // ---- Arrange
+
+        $book_author = 'J. R. R. Tolkien';
+        $book_genre = 'Fantasy';
+
+        // Creates the post
+        $post_id = wp_insert_post( array(
+            'post_type' => 'book',
+            'post_title' => 'The Lord Of The Rings'
+        ));
+
+        // Add the custom fields
+        update_post_meta($post_id, 'author', $book_author);
+        update_post_meta($post_id, 'genre', $book_genre);
+
+        // ---- Act
+        $book = $this->app->book->find();
+
+        // ---- Assert
+        $this->assertInstanceOf('PostObject', $book);
+        $this->assertEquals($book_author, $book->author, 'Asserts if the PostObject returns correctly the attribute author');
+        $this->assertEquals($book_genre, $book->genre, 'Asserts if the PostObject returns correctly the attribute genre');
 
     }
 
