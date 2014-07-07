da50d2c0ec6e6a940f24e3e4cc3314ebacbb407b
Sérgio Vilar <sergiovilar.r@gmail.com>
2014-07-07 17:58:43 -0300

    Refactor and added some tests

--- test/spec/test_models.php
+++ test/spec/test_models.php
@@ -21,7 +21,7 @@ class WP_Test_Alter_Models extends Alter_UnitTestCase {
     /**
      * This method tests if the post type has been registered
      */
-    function test_post_type_exists(){
+    function test_models_post_type_exists(){
 
         do_action('init');
         $this->assertTrue(post_type_exists('book'), 'Verify if post type "book" has been registered');
@@ -41,7 +41,7 @@ class WP_Test_Alter_Models extends Alter_UnitTestCase {
     /**
      * This method test the attributes of the PostObject
      */
-    function test_models_return(){
+    function test_models_find(){
 
         // ---- Arrange
 
@@ -59,7 +59,7 @@ class WP_Test_Alter_Models extends Alter_UnitTestCase {
         update_post_meta($post_id, 'genre', $book_genre);
 
         // ---- Act
-        $book = $this->app->book->find($post_id);
+        $book = $this->app->book->findById($post_id);
 
         // ---- Assert
         $this->assertTrue(is_int($post_id), 'Assert if wp_insert_post returns a post id');
@@ -68,5 +68,103 @@ class WP_Test_Alter_Models extends Alter_UnitTestCase {
         $this->assertEquals($book_genre, $book->genre, 'Asserts if the PostObject returns correctly the attribute genre');
 
     }
+
+	/**
+	 * This method tests the return of multiple results from a model
+	 */
+	function test_models_multiple_results(){
+
+		// ---- Arrange
+		$this->factory->post->create_many(10, array(
+			'post_type' => 'book'
+		));
+
+		// ---- Act
+		$books = $this->app->book->find();
+
+		// ---- Assert
+		$this->assertTrue(true, 'Asserts if the book->find() returns an array');
+		$this->assertInstanceOf('PostObject', $books[0], 'Asserts if is an array of PostObject');
+
+	}
+
+	/**
+	 * This method tests the return of multiple results with limit from a model
+	 */
+	function test_models_find_with_limit(){
+
+		// ---- Arrange
+		$this->factory->post->create_many(10, array(
+			'post_type' => 'book'
+		));
+
+		// ---- Act
+		$books = $this->app->book->find(5);
+		$books_wpquery = $this->app->book->find(array('posts_per_page' => 5));
+
+		// ---- Assert
+		$this->assertTrue(is_array($books), 'Assert if book->find(5) returns an array');
+		$this->assertEquals(count($books), 5, 'Assert if the lenght is correct');
+		$this->assertEquals(count($books_wpquery), 5, 'Assert if the lenght is correct');
+
+	}
+
+	/**
+	 * This method tests the paginate method of the model
+	 */
+	function test_models_paginate(){
+
+		// ---- Arrange
+		$this->factory->post->create_many(100, array(
+			'post_type' => 'book'
+		));
+
+		// ---- Act
+		$books_page1 = $this->app->book->paginate(10);
+		$books_page2 = $this->app->book->paginate(10, 2);
+
+		// ---- Assert
+		$this->assertTrue(is_array($books_page1), 'Assert if book->find(5) returns an array');
+		$this->assertEquals(count($books_page1), 10, 'Assert if the lenght is correct');
+		$this->assertInstanceOf('PostObject', $books_page1[0], 'Asserts if is an array of PostObject');
+
+		$this->assertTrue(is_array($books_page2), 'Assert if book->find(5) returns an array');
+		$this->assertEquals(count($books_page2), 10, 'Assert if the lenght is correct');
+		$this->assertInstanceOf('PostObject', $books_page2[0], 'Asserts if is an array of PostObject');
+
+		$this->assertNotEquals($books_page1[0]->ID, $books_page2[0]->ID, 'Asserts if the two arrays are not equals');
+
+	}
+
+	/**
+	 * This method tests the result of finding by a slug on the model
+	 */
+	function test_models_find_by_slug(){
+
+		// ---- Arrange
+		$title = 'The Lord Of The Rings';
+
+		$this->factory->post->create( array(
+			'post_type' => 'book',
+			'post_title' => $title
+		));
+
+		// ---- Act
+		$book = $this->app->book->findBySlug('the-lord-of-the-rings');
+
+		// ---- Assert
+		$this->assertInstanceOf('PostObject', $book);
+		$this->assertEquals($book->title, $title);
+
+	}
+
+	/**
+	 * This method tests the result of finding by a taxonomy on the model
+	 */
+	function test_models_find_by_taxonomy(){
+
+		// TODO: Register some taxonomies on example-theme
+
+	}
 
 }
